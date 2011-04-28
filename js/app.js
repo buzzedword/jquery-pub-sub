80230d942c9d2b849a9496c516ebdfaa1db94c78
Daniel Garcia <dgarc87@gmail.com>
2011-04-28 17:00:52 +0000

    Comments.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
 var ns = {}, users = {};
 
+// This class creates a Twitter instance to subscribe to.
+// @param selector | jQuery selector to update status against.
+// @param handler | string to use as an event handler in amplify. Request, Subscribe, and Publish events are mapped to this.
+// @param user | Twitter user to create.
+
 function GetTwitterUser( selector, handler, user ) {
     var interval, map = {}, count;
-        
+/* Constructors */    
+    // This set will predefine the AJAX call and cache with whatever client side caching is available.
     amplify.request.define( handler, "ajax", {
         url: "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=" + user + "&callback=?",
         dataType: "json",
-        type: "GET"
+        type: "GET",
+        cache: "persist"
     });
+    
+    // This is a global subscription to the GetTwitterUser class actions.
     amplify.subscribe( handler, function( payload ){
         if (typeof payload.incoming !== 'undefined') {
             selector.append('<p>' + payload.incoming + '</p>');
@@ -16,11 +25,14 @@ function GetTwitterUser( selector, handler, user ) {
         }
     });
     
+    // This executes the AJAX request previously defined. TODO add polling.
     amplify.request( handler, function(data) {
         map.data = data;
         map.count = data.length;
     });
-    
+/* End Constructors */
+/* Public functions */
+    // This will begin emitting updates one at a time with the given constraints from the constructor, and will reset after all have incremented.
     this.startPublishing = function( time ){
         count = 0;
         interval = setInterval(function(){
@@ -29,6 +41,7 @@ function GetTwitterUser( selector, handler, user ) {
             count++;
         }, ((typeof time !== 'undefined')? time : 3000));   
     };
+    // This is a destruction function for emitting events. TODO add complete destruction methods.
     this.stopPublishing = function(){
         clearInterval(interval);
         amplify.publish( handler, { 'destroy' : true});
@@ -36,4 +49,4 @@ function GetTwitterUser( selector, handler, user ) {
 }
 
 users.buzzedword = new GetTwitterUser( $('#buzzedword'), "twitter", "buzzedword");
-users.buzzedword.startPublishing();
+users.buzzedword.startPublishing(); /* This method also takes an interval as a parameter. */
